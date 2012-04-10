e5f5e6f0168157ebcb7543b5bdd59cdf49dda4ba
Anders Janmyr <anders@janmyr.com>
2012-04-10 08:51:22 +0200

    Replaced eql and should with chai

--- test/schedule-test.js
+++ test/schedule-test.js
@@ -1,9 +1,10 @@
 describe('Schedule', function() {
     "use strict";
-    
+    var test =$('#schedule').schedule().test;
+
     describe('#sanity()', function() {
         it('returns sane', function() {
-            $.schedule.test.sanity().should.eql('sane');
+            test.sanity().should.equal('sane');
         });
     });
 });
